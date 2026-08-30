979f2aaabf1c1e4bfc05f0b69f8d2d506464d0c3
CUISI <cuisi@users.noreply.local>
2026-08-30 14:01:59 +0800

    生活板块填充 8 张真实照片

--- src/data/life.ts
+++ src/data/life.ts
@@ -5,8 +5,12 @@ export interface LifePhoto {
 }
 
 export const lifePhotos: LifePhoto[] = [
-  { title: "生活照片 1", date: "2026", image: "/life/life-1.svg" },
-  { title: "生活照片 2", date: "2026", image: "/life/life-2.svg" },
-  { title: "生活照片 3", date: "2026", image: "/life/life-3.svg" },
-  { title: "生活照片 4", date: "2026", image: "/life/life-4.svg" },
+  { title: "生活照片 1", date: "2026-08-30", image: "/life/life-1.jpg" },
+  { title: "生活照片 2", date: "2026-08-30", image: "/life/life-2.jpg" },
+  { title: "生活照片 3", date: "2026-08-30", image: "/life/life-3.jpg" },
+  { title: "生活照片 4", date: "2026-08-30", image: "/life/life-4.jpg" },
+  { title: "生活照片 5", date: "2026-08-30", image: "/life/life-5.jpg" },
+  { title: "生活照片 6", date: "2026-08-30", image: "/life/life-6.jpg" },
+  { title: "生活照片 7", date: "2026-08-30", image: "/life/life-7.jpg" },
+  { title: "生活照片 8", date: "2026-08-30", image: "/life/life-8.jpg" },
 ];
